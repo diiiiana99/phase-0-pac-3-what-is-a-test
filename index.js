d983b0285c5756c022289f27ad5b749f6b9906ef
diiiiana99 <93669061+diiiiana99@users.noreply.github.com>
2021-11-24 15:54:03 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const name = "Joe";
+/*const name = "Joe";
+const name2 = "Chris";
 const height = 74;
-const message = `${name} is ${height} inches tall`;
+const age = 30;
+const message = `${name}  is ${height} inches tall;
+ ${name2} is ${age} old`;
+
 
 module.exports = {name, height, message};
+console.log (message);
+*/
+const name = "Susan";
+const height = "less than 40";
+const message = `${name} is ${height} to be less than`;
--- test/index-test.js
+++ test/index-test.js
@@ -1,15 +1,11 @@
 const { name, height, message } = require("../index.js");
 
-/*
-describe('what-is-a-test', () => {
-  describe('Name', () => {
-    it('returns "Susan"', () => {
-      expect(name).toEqual('Susan')
-    })
-  })
+const name = "Susan"
+const height = "less than 40"
+const message = `${name} is ${height} to be less than`;
+ 
 
-
-  describe('Height', () => {
+ describe('Height', () => {
     it('is less than 40', () => {
       expect(height).toBeLessThan(40)
     })
